2e748dd647e30e635f094d0a5a5123753e094478
Erin Atkinson <20285458+erindatkinson@users.noreply.github.com>
2024-03-25 11:24:07 -0600

    pull out css per component
    Signed-off-by: Erin Atkinson <20285458+erindatkinson@users.noreply.github.com>

--- web/src/components/Form.jsx
+++ web/src/components/Form.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-
+import './Form.css';
 
 function Form(props) {
     const [name, setName] = useState("");
--- web/src/components/Todo.jsx
+++ web/src/components/Todo.jsx
@@ -1,4 +1,5 @@
 import { useState, useRef, useEffect } from "react";
+import './Todo.css';
 
 function usePrevious(value) {
     const ref = useRef();
